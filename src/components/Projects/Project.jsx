bae3c5666000241288b38ed022495c77f32f912d
E-commerce <shubhamramane143@gmail.com>
2026-02-08 15:53:28 +0530

    later adding projects

---
+++ src/components/Projects/Project.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Project = () => {
+  return (
+    <>
+      <span
+        className="flex justify-center text-2xl md:text-4xl lg:text-5xl font-semibold  text-white/75"
+        style={{
+          marginTop: "5rem",
+          marginBottom: "2rem",
+        }}
+      >
+        Projects
+      </span>
+      <div
+        className="md:text-2xl lg:text-xl font-semibold leading-tight tracking-[-0.02em] text-white/75 max-w-10/12"
+        style={{
+          margin: "2rem",
+        }}
+      >
+        Projects are currently under development and will be added to this portfolio soon.
+      </div>
+    </>
+  );
+};
+
+export default Project;
