8ee37dc1edb677e0943080eb4fa2c5baf25b3764
kaushikp092 <kaushikpatil092@gmail.com>
2026-02-10 20:34:02 +0530

    added in github and linkedin icons in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,40 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-   return (
-      <>
-         <div
-            className="inset-x-0 bottom-0 w-full flex flex-col md:flex-row justify-evenly  items-center text-white text-xs md:text-xl"
-            style={{
-               height: "70px",
-               backdropFilter: "blur(12px)",
-               background: "rgba(36, 37, 46, 0.3)",
-               zIndex: 10,
-            }}
-         >
-            <p>
-            Designed and Developed with ❤️ by @Kaushik Patil
-            </p>
-            <p>Copyright ©️ Kaushik 2026</p>
-         </div>
-      </>
-   );
+  return (
+    <>
+      <div
+        className="inset-x-0 bottom-0 w-full flex flex-col md:flex-row justify-around  items-center text-white text-xs md:text-xl"
+        style={{
+          height: "70px",
+          backdropFilter: "blur(12px)",
+          background: "rgba(36, 37, 46, 0.3)",
+          zIndex: 10,
+        }}
+      >
+        <p>Designed and Developed with ❤️ by @Kaushik Patil</p>
+        <p>Copyright ©️ Kaushik 2026</p>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/kaushik-patil-644553343"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a
+            href="https://github.com/Kaushikp092"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
