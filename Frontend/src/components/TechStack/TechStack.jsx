dfccda6d27f887db64aeb5da69adf5e079e82843
kaushikp092 <kaushikpatil092@gmail.com>
2026-03-23 00:12:16 +0530

    added tech stack to motion to give slider animation

--- Frontend/src/components/TechStack/TechStack.jsx
+++ Frontend/src/components/TechStack/TechStack.jsx
@@ -1,3 +1,6 @@
+import { cn } from '@/lib/utils';
+import { InfiniteSlider } from '../../../components/motion-primitives/infinite-slider';
+
 const TechStack = () => {
 	const logos = [
 		{ title: "HTML", img: "/html.svg" },
@@ -12,24 +15,36 @@ const TechStack = () => {
 		{ title: "RENDER", img: "/render.svg" },
 	];
 	return (
-		<div>
-			<h2 className="text-center text-white/75 text-2xl md:text-4xl lg:text-5xl font-semibold mb-8">
+		<div className="w-screen flex flex-col items-center justify-center" style={{ paddingTop: '5rem', paddingBottom: '2rem', paddingLeft: '2rem', paddingRight: '2rem' }}>
+			<span className="flex justify-center text-2xl md:text-4xl lg:text-5xl font-semibold text-white/75" style={{ marginBottom: '4rem' }}>
 				Tech Stack
-			</h2>
-			<div className="flex flex-wrap justify-center gap-6">
+			</span>
+			<div style={{ marginBottom: '2rem' }} />
+			<InfiniteSlider
+				speed={65}
+				speedOnHover={40}
+				gap={40}
+				className={cn("w-full py-20 md:py-24")}
+			>
 				{logos.map((logo) => (
 					<div
 						key={logo.title}
-						className="bg-white/10 backdrop-blur-md border border-white/20 rounded-lg p-6 flex items-center gap-3 hover:scale-105 transition"
+						className="group bg-linear-to-br from-white/5 via-black/30 to-white/5 backdrop-blur-xl border border-white/10 rounded-xl p-8 md:p-10 flex items-center justify-center gap-6 mx-3 md:mx-4 lg:mx-6 shadow-2xl shadow-black/20 hover:scale-110 hover:-translate-y-2 hover:shadow-2xl transition-all duration-500 cursor-pointer min-h-30 md:min-h-35 min-w-45 md:min-w-50 shrink-0"
 					>
-						<img src={logo.img} alt={logo.title} className="h-8 w-auto" />
-
-						<span className="text-white text-sm font-medium">{logo.title}</span>
+						<img
+							src={logo.img}
+							alt={logo.title}
+							className="h-9 md:h-11 lg:h-13 w-auto shrink-0 drop-shadow-lg group-hover:drop-shadow-2xl transition-all"
+						/>
+						<span className="text-white/90 text-sm md:text-base font-semibold transition-colors duration-300 tracking-wide">
+							{logo.title}
+						</span>
 					</div>
 				))}
-			</div>
+			</InfiniteSlider>
 		</div>
 	);
 };
 
 export default TechStack;
+
