d51aace8521b59ccceae52a30156fc1a259fbe56
kaushikp092 <kaushikpatil092@gmail.com>
2026-03-23 13:57:25 +0530

    motion added to projects card

--- Frontend/src/components/Projects/ProjectCard.jsx
+++ Frontend/src/components/Projects/ProjectCard.jsx
@@ -1,3 +1,4 @@
+import { InView } from "../ui/in-view";
 import "./ProjectCard.css";
 
 const ProjectCard = ({
@@ -9,25 +10,42 @@ const ProjectCard = ({
 	githubLink,
 }) => {
 	return (
-		<div className="project-card">
-			<img src={image} alt={title} className="project-image" />
-			<div className="project-content">
-				<h3>{title}</h3>
-				<p className="project-desc">{description}</p>
-				<p className="project-tech">
-					<strong>Tech:</strong>
-					{tech}
-				</p>
-				<div className="project-links">
-					<a href={liveLink} target="_blank" rel="noopener noreferrer">
-						Live Demo
-					</a>
-					<a href={githubLink} target="_blank" rel="noopener noreferrer">
-						Github
-					</a>
+		<>
+			<InView
+				variants={{
+					hidden: {
+						opacity: 0,
+						x: 100,
+					},
+					visible: {
+						opacity: 1,
+						x: 0,
+					},
+				}}
+				transition={{ duration: 0.3, ease: "easeInOut" }}
+				viewOptions={{ margin: "0px 0px -350px 0px" }}
+			>
+				<div className="project-card">
+					<img src={image} alt={title} className="project-image" />
+					<div className="project-content">
+						<h3>{title}</h3>
+						<p className="project-desc">{description}</p>
+						<p className="project-tech">
+							<strong>Tech:</strong>
+							{tech}
+						</p>
+						<div className="project-links">
+							<a href={liveLink} target="_blank" rel="noopener noreferrer">
+								Live Demo
+							</a>
+							<a href={githubLink} target="_blank" rel="noopener noreferrer">
+								Github
+							</a>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			</InView>
+		</>
 	);
 };
 
